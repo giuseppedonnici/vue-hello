f84377cfdb2e35ee2de2d350e2371ba1ac520117
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-03-28 13:19:46 +0200

    Add: dynamic h1 and image

--- js/script.js
+++ js/script.js
@@ -9,7 +9,10 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            message: 'Welcome Vue JS!',
+            color: 'white',
+            imgSrc: 'https://www.freecodecamp.org/news/content/images/size/w2000/2022/08/Vue-Blog-Cover-2.png',
+            imgAlt: 'Immagine di Vue JS'
         }
     },
     methods: {
