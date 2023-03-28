6464d7b6e42fa3b8b677b35a87008e13003b54c5
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-03-28 16:57:17 +0200

    Add: buttons and input for fun

--- js/script.js
+++ js/script.js
@@ -10,12 +10,30 @@ createApp({
     data() {
         return {
             message: 'Welcome Vue JS!',
+            text: 'Username',
             color: 'white',
+            yellow: 'Yellow',
+            green: 'Green',
+            chooise: 'white',
             imgSrc: 'https://www.freecodecamp.org/news/content/images/size/w2000/2022/08/Vue-Blog-Cover-2.png',
             imgAlt: 'Immagine di Vue JS'
         }
     },
     methods: {
+        handleKeyup() {
+            console.log("Hai digitato un tasto");
+        },
 
+        changeToYellow() {
+            if(this.chooise === 'white' || this.chooise === 'green') {
+                this.chooise = 'yellow';
+            }
+        },
+
+        changeToGreen() {
+            if(this.chooise === 'white' || this.chooise === 'yellow') {
+                this.chooise = 'green';
+            }
+        }
     }
 }).mount("#app");
